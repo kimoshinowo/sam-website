f77b495af5e32419e10a1f4ef64c710e54d0c59b
kimoshinowo <kimoshinowo@hotmail.co.uk>
2024-08-02 15:39:06 +0100

    Add base site styles and content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <header className="masthead">
+            <div className="masthead-name">
+                <h1>Samuel</h1>
+            </div>
+
+            <Nav />
+        </header>
+    );
 }
 
 export default App;
