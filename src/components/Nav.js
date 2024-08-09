4b4916c503014d4cfeaad8d8585efe857cc11984
kimoshinowo <kimoshinowo@hotmail.co.uk>
2024-08-09 15:10:45 +0100

    Move all nav stuff to one component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,16 @@
 import { useState } from 'react';
+import '../styles/Hamburger.css';
 import '../styles/Nav.css';
-import Hamburger from './Hamburger';
+
+function Hamburger({ isOpen }) {
+    return (
+        <div className="hamburger">
+            <div className={"burger " + (isOpen ? 'burger1-open' : '')}></div>
+            <div className={"burger " + (isOpen ? 'burger2-open' : '')}></div>
+            <div className={"burger " + (isOpen ? 'burger3-open' : '')}></div>
+        </div>
+    );
+}
 
 const Nav = () => {
     const [hamburgerOpen, setHamburgerOpen] = useState(false);
